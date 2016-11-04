59c902502968bd1b4d0377a51b50fd9d600b6105
Luis Arturo Pacheco <luigipach@gmail.com>
2016-11-04 16:07:55 -0600

    added user roles

--- meteor/MakerERP/makererp/imports/ui/task.js
+++ meteor/MakerERP/makererp/imports/ui/task.js
@@ -4,11 +4,15 @@ import './task.html'
 
 Template.task.events({
   'click .toggle-checked'(){
+    if (Meteor.user().username == this.username || Meteor.user().username == "luigi"){
     Tasks.update(this._id, {
     $set: { checked: ! this.checked },
   });
+  }
 },
   'click .delete'(){
+    if (Meteor.user().username == "luigi"){
     Tasks.remove(this._id);
+  }
   },
 });
