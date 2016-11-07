77f2952485e496fd6653ceb9133cf72497699e87
Luis <luigipach@gmail.com>
2016-11-07 17:42:14 -0600

    hide x from ui

--- imports/startup/accounts-config.js
+++ imports/startup/accounts-config.js
@@ -3,3 +3,11 @@ import {Accounts} from 'meteor/accounts-base';
 Accounts.ui.config({
   passwordSignupFields :'USERNAME_ONLY',
 });
+
+Accounts.createUser({
+                            username: "admin",
+                            password :"admin123",
+    });
+//var adminuser = Accounts.findUserByUsername("admin")._id;
+
+//Roles.addUsersToRoles( Accounts.findUserByUsername("admin")._id, ['admin'] );
